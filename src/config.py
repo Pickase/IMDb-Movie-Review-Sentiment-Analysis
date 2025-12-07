35a6e220a9ba48cc135ecea29c82940ee5f6b0c7
Pranav Joshi <jpranav99.pj@gmail.com>
2025-12-07 22:13:50 +0530

    Update RAW_DATA_PATH to Google Sheets link

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "models", "sentiment_model.pkl")
 
 # Used only during local training
-RAW_DATA_PATH = "YOUR_GOOGLE_DRIVE_LINK"
+RAW_DATA_PATH = "https://docs.google.com/spreadsheets/d/1gblqnEpfJPCeTX_a5v-4hPKomhF_Ozpq/edit?usp=sharing&ouid=117900499454272180226&rtpof=true&sd=true"
 PROCESSED_DATA_PATH = os.path.join(BASE_DIR, "processed_local.csv")  # local only
 
 REVIEW_COL = "review"
